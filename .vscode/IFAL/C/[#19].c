a2c31f7aeb919dbaa61bbde32e129f180c579cd4
D3llzu <cgga1@aluno.ifal.edu.br>
2025-02-04 08:21:31 -0300

    #

--- .vscode/IFAL/C/[#19].c
+++ .vscode/IFAL/C/[#19].c
@@ -1,2 +1,23 @@
 /*[#4] Leia um nome completo de pessoa e imprima apenas o nome e sobrenome no formato SOBRENOME/Nome. 
 Ex.: Antônio Fernando Silva → SILVA/Antônio*/
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+int main(){
+    char Nome[20], SNome[20]; SNOME[20]
+
+    printf("Digite seu primeiro nome: ");
+    gets (Nome);
+
+    printf("Digite agora seu sobrenome: ");
+    gets (SNome);
+
+    SNOME[20] = toupper(SNome[20]);
+
+    printf("\n", "%s", SNome, "/%s", Nome);
+
+    return 0;
+}
--- .vscode/IFAL/Python/[#20].py
+++ .vscode/IFAL/Python/[#20].py
@@ -1,1 +1,9 @@
 """[#5] Receba duas frases distintas e imprima de maneira invertida, trocando as letras A por *."""
+
+frase1 = (input("Digite uma frase aqui: "))[::-1]
+frase2 = (input("Digite outra frase agora: ")[::-1])
+
+frase3 = frase1.replace ("a", "*")
+frase4 = frase2.replace ("a", "*")
+
+print = (frase3, frase4)
