01c38ccadcceed4badc867f3e90e0cee65475c6f
D3llzu <cgga1@aluno.ifal.edu.br>
2025-03-07 07:35:47 -0300

    Add files via upload

---
+++ .vscode/.py/tst1.py
@@ -0,0 +1,16 @@
+for count in range (10, 0, -1):
+    print(f"{count}")
+    
+print ("\nFogo!")
+
+'''#include <stdio.h>
+#include <unistd.h>
+int main(){
+    int count;
+    
+    for (count = 10; count >= 1; count--){
+        printf ("%i\n", count);
+        sleep (1);}
+        printf("\nFogo!");
+    return 0;
+}'''
